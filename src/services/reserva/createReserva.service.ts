6254db7b6fc8fa5763e281992106618ff92d1828
Caio <caio.faria@realizati.com.br>
2022-09-14 22:11:14 -0300

    add: services de  reservas

---
+++ src/services/reserva/createReserva.service.ts
@@ -0,0 +1,42 @@
+import Reserva from "../../entity/Reserva";
+import { ReservaRepository } from "../../repositories/reserva.repository";
+import AppError from "../../shared/errors/AppError";
+import CheckQuartoStatus from "./checkQuartoStatus.service";
+
+interface IRequest {
+  nome_hotel: string;
+  numero_do_quarto: string;
+  valor_reserva: any;
+  data_checkin: any;
+  data_checkout: any;
+  status_reserva: string;
+}
+
+class CreateReservaService {
+  public async execute(data: IRequest): Promise<Reserva> {
+
+    const {numero_do_quarto } = data
+    
+    const checkQuartoStatus = new CheckQuartoStatus
+    const ocupado = await checkQuartoStatus.execute({numero_do_quarto})
+    
+    if(ocupado) {
+      throw new AppError("Quarto se encontra ocupado.", 403);
+    }
+
+    const reserva = await ReservaRepository.create({
+      nome_hotel: data.nome_hotel,
+      numero_do_quarto: data.numero_do_quarto,
+      valor_reserva: data.valor_reserva,
+      data_checkin: data.data_checkin,
+      data_checkout: data.data_checkout,
+      status_reserva: "Confirmada",
+    })
+
+    await ReservaRepository.save(reserva)
+
+    return reserva
+  }
+}
+
+export default CreateReservaService;
